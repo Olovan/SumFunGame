56614b84ec0a260a2d3fbb560625d038ddb808d1
Quentin Lintz <lintqm01@students.ipfw.edu>
2017-10-21 13:51:15 -0400

    Added CalcScore and UpdateGrid methods

--- src/SumFunBoardLogic.java
+++ src/SumFunBoardLogic.java
@@ -1,4 +1,3 @@
-
 public class SumFunBoardLogic 
 {
 	private Controller c;
@@ -30,5 +29,62 @@ public class SumFunBoardLogic
 		testGrid[row][col] = 7;
 		
 		c.returnCoordinateValue(testGrid[row][col], row, col);
+	}
+	
+	// Score is returned based on how many tiles are removed and the boundary sum
+	public int CalcScore(int row, int col, int placement, Integer[][] grid) {
+		int boundarySum = 0;
+		int tilesRemoved = 0;
+		
+		// Sums the 3x3 square with "placement" in the middle
+		if (grid[row][col] == null) {
+			for  (int i = -1; i <= 1; i++) {
+				for (int j = -1; j <= 1; j++) {
+					if (BoundaryCheck(row + i, col + j) == true) {
+						boundarySum += grid[row + i][col + j];
+						if (grid[row + i][col + j] != null)
+							tilesRemoved++;
+					}
+				}
+			}
+		}
+		
+		// The algorithm above will include the middle number
+		// We want to remove this for the next calculation
+		boundarySum -= placement;
+		
+		
+		if (boundarySum % 10 == placement) {
+			if (tilesRemoved >= 3) {
+				boundarySum += 10 * tilesRemoved;   
+			}
+			
+			return boundarySum;
+		}
+		else
+			return 0;
+	}
+	
+	// This is ONLY called if the placement will remove the surrounding blocks
+	// Returns the updated grid with removed spaces 
+	public Integer[][] UpdateGrid(int row, int col, Integer[][] grid) {
+		for  (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				if (BoundaryCheck(row + i, col + j) == true) {
+					grid[row + i][col + j] = null;
+				}
+			}
+		}
+		
+		return grid;
+	}
+	
+	// Returns true if the row/column combination is within bounds
+	// This can be changed later if the size of the board changes
+	public boolean BoundaryCheck(int r, int c) {
+		if (r >= 0 && r <= 8 && c >= 0 && c <= 8)
+			return true;
+		else
+			return false;
 	}
 }
