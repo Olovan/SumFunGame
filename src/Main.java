6d1e1fcfe997028302fa6d11e6c40e57ab7de428
Olovan <micahsmith.92@gmail.com>
2017-11-18 16:34:03 -0500

    Removed TestHighScoreLogic class as it's no longer needed

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 	public static void main(String[] args) {
 		SumFunMainGui view = new SumFunMainGui();
 		SumFunLeaderboardGui leaderboard = new SumFunLeaderboardGui();
-		TestHighScoreLogic test = new TestHighScoreLogic();
 		SumFunHighScoreLogic.getInstance().loadFromFile();
 		view.setVisible(true);
 	}
