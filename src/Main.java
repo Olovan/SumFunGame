56944df8c4eac1da774e1427d520352bb35ea5f3
Olovan <micahsmith.92@gmail.com>
2017-11-20 12:50:15 -0500

    Changed Look and Feel to System default

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import javax.swing.UIManager;
+
 public class Main {
 	public static void main(String[] args) {
+		try { 
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		SumFunMainGui view = new SumFunMainGui();
 		new SumFunLeaderboardGui();
 		view.setVisible(true);
